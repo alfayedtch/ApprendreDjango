49e2ea25d3d4bc5d0d920e3801e542f9057a78d7
alfayedtch <albos7484@gmail.com>
2022-02-03 17:21:36 +0100

    creation des migration et creation du super admin/ creation of migrations and creation of superadmin

--- ApprendreDjango/settings.py
+++ ApprendreDjango/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'ApprendreDjango.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db1.sqlite3',
     }
 }
 
