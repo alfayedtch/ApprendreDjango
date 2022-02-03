cf73b86322d4cba0ade012f122cd1ad6ae449197
alfayedtch <albos7484@gmail.com>
2022-02-03 19:43:35 +0100

    ajout des templates/ Add of templates

--- vue/views.py
+++ vue/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>slt</h1>")
+    data = {'nom':'TCHAGNAO','prenom':'al-fayed', 'logiciel':['word','excel','powerPoint'],'age':19 }
+    return HttpResponse(loader.get_template('index.html').render(data))
